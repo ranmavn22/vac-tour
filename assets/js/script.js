35134388306d477b549b73464528862891ed37ed
Thi Nguyen <thinguyen.dev@gmail.com>
2019-12-13 02:56:58 +0700

    u

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 (function ($) {
     //$('input[name="tesst2"]').val($('.attachmentUrl').html());
+    $('.toggetInforTour h3').on('click', function (e) {
+        let el = $(this).closest('.gdlr-core-pbf-element');
+        if(el.hasClass('active')){
+            el.removeClass('active');
+        }else{
+            el.addClass('active');
+        }
+
+    })
 }(jQuery));
